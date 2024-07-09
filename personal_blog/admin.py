1a808aa5f0f16345ee5c64842d13b8dfded1fcc5
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-07-09 18:28:20 +0200

    Add admin interface

--- personal_blog/admin.py
+++ personal_blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "created_date", "published_date")
+    list_filter = ("created_date",)
+    search_fields = ("created_date", "published_date")
+admin.site.register(Post, BlogAdmin)
