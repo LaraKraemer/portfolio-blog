c4c06b9a205ea95542aff2312f75d5828ca42a25
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-07-29 10:38:58 +0200

    Adjusting static file organization

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,10 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'personal_blog/static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'mystaticfiles'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
